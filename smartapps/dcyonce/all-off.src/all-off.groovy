043f229a94a0e817a3e617c1ee4b321626f72e67
DON YONCE <dcyonce@yahoo.com>
2017-08-26 00:19:51 -0500

    Initial load

---
+++ smartapps/dcyonce/all-off.src/all-off.groovy
@@ -0,0 +1,51 @@
+/**
+ *  All Off
+ *
+ *  Copyright 2017 DON YONCE
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "All Off",
+    namespace: "dcyonce",
+    author: "DON YONCE",
+    description: "Turn all lights OFF when a button is pressed twice",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section("Which Button?") {
+	    input name: "button", type: "capability.sensor", title: "Button"
+	    input name: "Actuator", type: "capability.actuator", title: "Actuator"
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
---
+++ smartapps/dcyonce/turn-on-lights.src/turn-on-lights.groovy
@@ -0,0 +1,41 @@
+/**
+ *  Turn on lights
+ *
+ *  Copyright 2017 DON YONCE
+ *
+ */
+definition(
+    name: "Turn on lights",
+    namespace: "dcyonce",
+    author: "DON YONCE",
+    description: "Turn on lights from motion, or door openings",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section("Title") {
+		// TODO: put inputs here
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
